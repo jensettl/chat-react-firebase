51b49d3c3474866662a6fae4e5e10abf76df69a5
Jens Ettl <ettljens@gmail.com>
2022-10-17 10:52:42 +0200

    added firebase configuration

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,44 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { initializeAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAuJzWaR9npz8-eC9qtWdczwnMlcQCR3qI",
+  authDomain: "test-firebase-33a57.firebaseapp.com",
+  projectId: "test-firebase-33a57",
+  storageBucket: "test-firebase-33a57.appspot.com",
+  messagingSenderId: "120046803559",
+  appId: "1:120046803559:web:96fb70be8c8aa8457f92c2",
+  measurementId: "G-SLNENE422F"
+};
+
+firebase.initializeApp({
+  firebaseConfig
+})
+
+// const app = initializeApp(firebaseConfig);
+// const analytics  = getAnalytics(app);
+
+const auth = firebase.auth();
+const firestore = firebase.firestore()
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
+
+      <section>
+
+      </section>
     </div>
   );
 }
